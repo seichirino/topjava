3278b2cd2cca08055789481ffc45109840322d09
Seichirino <likeanopenwindow@gmail.com>
2019-01-26 23:31:47 +1000

    Add toString, make ineffective double cycle solution

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,8 +6,8 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.time.chrono.ChronoLocalDateTime;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,13 +19,30 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
+        for(UserMealWithExceed meal : getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000)){
+            System.out.println(meal);
+        }
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        //Double cycle variant O(N*N)
+        List<UserMealWithExceed> result = new ArrayList<>();
+        Map<LocalDateTime,Integer> exceed = new HashMap<>();
+        for(UserMeal meal : mealList){
+            exceed.merge(meal.getDateTime(), meal.getCalories(), (a, b) -> a + b);
+        }
+        for(UserMeal meal : mealList){
+            if(LocalTime.from(meal.getDateTime()).isBefore(endTime) && LocalTime.from(meal.getDateTime()).isAfter(startTime)){
+                if(exceed.get(meal.getDateTime().getDayOfYear()) > caloriesPerDay){
+                    result.add(new UserMealWithExceed(meal.getDateTime(),meal.getDescription(),meal.getCalories(),true));
+                }else{
+                    result.add(new UserMealWithExceed(meal.getDateTime(),meal.getDescription(),meal.getCalories(),false));
+                }
+            }
+        }
+        return result;
     }
 }
