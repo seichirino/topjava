a80b7a8ba489582c25b499a93a6fcc52a3a52af6
Seichirino <likeanopenwindow@gmail.com>
2019-02-07 18:08:39 +1000

    HW0 done

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.time.chrono.ChronoLocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,30 +20,39 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        for(UserMealWithExceed meal : getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000)){
-            System.out.println(meal);
-        }
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+
+        List<UserMealWithExceed> filteredWithExceeded = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExceed> filteredWithExceededCycle = getFilteredWithExceededByCycle(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        filteredWithExceeded.forEach(System.out::println);
+        filteredWithExceededCycle.forEach(System.out::println);
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        //Double cycle variant O(N*N)
-        List<UserMealWithExceed> result = new ArrayList<>();
-        Map<LocalDateTime,Integer> exceed = new HashMap<>();
+
+        //Stream variant O(N*2)
+        Map<LocalDate, Integer> caloriesSumByDate = mealList.stream()
+                .collect(Collectors.groupingBy(um -> um.getDateTime().toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
+        return mealList.stream()
+                .filter(um -> TimeUtil.isBetween(um.getDateTime().toLocalTime(),startTime,endTime))
+                .map(um -> new UserMealWithExceed(um.getDateTime(),um.getDescription(),um.getCalories(),
+                        caloriesSumByDate.get(um.getDateTime().toLocalDate()) > caloriesPerDay))
+                .collect(Collectors.toList());
+    }
+
+    public static List<UserMealWithExceed>  getFilteredWithExceededByCycle(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        //Double cycle variant O(2*N)
+
+
+        Map<LocalDateTime,Integer> caloriesSumByDate = new HashMap<>();
+        mealList.forEach(meal -> caloriesSumByDate.merge(meal.getDateTime(), meal.getCalories(), (a, b) -> a + b));
+
+        List<UserMealWithExceed> mealWithExceed = new ArrayList<>();
         for(UserMeal meal : mealList){
-            exceed.merge(meal.getDateTime(), meal.getCalories(), (a, b) -> a + b);
+            if(TimeUtil.isBetween(meal.getDateTime().toLocalTime(),startTime,endTime))
+                    mealWithExceed.add(new UserMealWithExceed(meal.getDateTime(),meal.getDescription(),meal.getCalories()
+                            ,caloriesSumByDate.get(meal.getDateTime()) > caloriesPerDay));
         }
-        for(UserMeal meal : mealList){
-            if(LocalTime.from(meal.getDateTime()).isBefore(endTime) && LocalTime.from(meal.getDateTime()).isAfter(startTime)){
-                if(exceed.get(meal.getDateTime().getDayOfYear()) > caloriesPerDay){
-                    result.add(new UserMealWithExceed(meal.getDateTime(),meal.getDescription(),meal.getCalories(),true));
-                }else{
-                    result.add(new UserMealWithExceed(meal.getDateTime(),meal.getDescription(),meal.getCalories(),false));
-                }
-            }
-        }
-        return result;
+        return mealWithExceed;
     }
 }
